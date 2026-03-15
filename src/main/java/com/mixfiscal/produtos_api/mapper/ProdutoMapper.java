eca71a10d21949db3fbe1582beab67873ae57d1b
Ricardo Silva <pricardo.sribeiro@gmail.com>
2026-03-15 13:02:05 -0300

    refactor: substitui produtomapper manual pelo mapstruct

--- src/main/java/com/mixfiscal/produtos_api/mapper/ProdutoMapper.java
+++ src/main/java/com/mixfiscal/produtos_api/mapper/ProdutoMapper.java
@@ -4,38 +4,16 @@ import com.mixfiscal.produtos_api.dto.in.ProdutoCreateRequest;
 import com.mixfiscal.produtos_api.dto.out.ProdutoResponse;
 import com.mixfiscal.produtos_api.dto.out.TaxResponse;
 import com.mixfiscal.produtos_api.entity.Produto;
-import org.springframework.stereotype.Component;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
-@Component
-public class ProdutoMapper {
+@Mapper(componentModel = "spring")
+public interface ProdutoMapper {
 
-    public Produto toDomain(ProdutoCreateRequest request) {
-        return Produto.builder()
-                .nome(request.getNome())
-                .sku(request.getSku())
-                .precoBase(request.getPrecoBase())
-                .codigoNcm(request.getCodigoNcm())
-                .build();
-    }
+    Produto toDomain(ProdutoCreateRequest produtoCreateRequest);
 
-    public ProdutoResponse toResponse(Produto produto) {
-        return ProdutoResponse.builder()
-                .id(produto.getId())
-                .nome(produto.getNome())
-                .sku(produto.getSku())
-                .precoBase(produto.getPrecoBase())
-                .codigoNcm(produto.getCodigoNcm())
-                .build();
-    }
+    ProdutoResponse toResponse(Produto produto);
 
-    public ProdutoResponse toResponse(Produto produto, TaxResponse taxResponse) {
-        return ProdutoResponse.builder()
-                .id(produto.getId())
-                .nome(produto.getNome())
-                .sku(produto.getSku())
-                .precoBase(produto.getPrecoBase())
-                .codigoNcm(produto.getCodigoNcm())
-                .imposto(taxResponse)
-                .build();
-    }
+    @Mapping(source = "taxResponse", target = "imposto")
+    ProdutoResponse toResponse(Produto produto, TaxResponse taxResponse);
 }
